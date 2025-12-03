22a1a719705dc7a1f05cd6f812fdbcd617821735
khguettler <knutguettler@gmail.com>
2025-12-03 11:19:35 +0100

    day 1: cleanup

--- day_1/main.go
+++ day_1/main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/khguettler/aoc_25/utils/conv"
 	"github.com/khguettler/aoc_25/utils/input"
 	"github.com/khguettler/aoc_25/utils/pretty"
@@ -20,54 +18,8 @@ func main() {
 	partTwo(lines)
 }
 
-func partTwo(lines []string) {
-	pretty.PartTwo()
-
-	count := 0
-	pos := 50
-	var prevPos int
-
-	for _, move := range lines {
-		dir := string(move[0])
-		num := conv.StrToInt(move[1:])
-
-		div := num / 100
-		count += div
-		num = num % 100
-		prevPos = pos
-
-		if dir == "L" {
-			pos -= num
-			pos = pos % 100
-			if pos < 0 {
-				pos = 100 + pos
-				if prevPos != 0 && pos != 0 {
-					count++
-				}
-			}
-		} else {
-			pos += num
-			div := pos / 100
-			_ = div
-
-			if pos > 99 {
-				pos = pos % 100
-				if prevPos != 0 && pos != 0 {
-					count++
-				}
-			}
-		}
-
-		if pos == 0 {
-			count++
-		}
-	}
-
-	pretty.Answer(count)
-}
-
 func partOne(lines []string) {
-	fmt.Printf("Part One!\n")
+	pretty.PartOne()
 
 	count := 0
 	pos := 50
@@ -87,6 +39,52 @@ func partOne(lines []string) {
 				pos = 100 + pos
 			}
 		}
+		if pos == 0 {
+			count++
+		}
+	}
+
+	pretty.Answer(count)
+}
+
+func partTwo(lines []string) {
+	pretty.PartTwo()
+
+	count := 0
+	pos := 50
+	var prevPos int
+
+	for _, move := range lines {
+		dir := string(move[0])
+		num := conv.StrToInt(move[1:])
+
+		div := num / 100
+		count += div
+
+		num = num % 100
+		prevPos = pos
+
+		if dir == "L" {
+			pos -= num
+			pos = pos % 100
+
+			if pos < 0 {
+				pos = 100 + pos
+				if prevPos != 0 && pos != 0 {
+					count++
+				}
+			}
+		} else {
+			pos += num
+
+			if pos > 99 {
+				pos = pos % 100
+				if prevPos != 0 && pos != 0 {
+					count++
+				}
+			}
+		}
+
 		if pos == 0 {
 			count++
 		}
